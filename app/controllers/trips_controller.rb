e2e4333fc1c3f8f3993affb19b26f90d22d67fe5
X_NAVID <navidcaesar@gmail.com>
2018-02-17 10:55:47 +0000

    make set_trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:edit, :update, :show, :destroy]
   def index
     @trips = Trip.all
   end
@@ -16,19 +17,19 @@ class TripsController < ApplicationController
     end
   end
   def show
-    @trip = Trip.find(params[:id])
+    #@trip = Trip.find(params[:id])
   end
   def edit
-    @trip = Trip.find(params[:id])
+    #@trip = Trip.find(params[:id])
   end
   def destroy
-    @trip = Trip.find(params[:id])
+    #@trip = Trip.find(params[:id])
     @trip.destroy
     flash[:notice] = "Trip successfully deleted !"
     redirect_to trips_path
   end
   def update
-    @trip = Trip.find(params[:id])
+    #@trip = Trip.find(params[:id])
     if @trip.update(trip_params)
       flash[:notice] = "Trip details updated !"
       redirect_to trip_path(@trip)
@@ -37,6 +38,9 @@ class TripsController < ApplicationController
     end
   end
   private
+    def set_trip
+      @trip = Trip.find(params[:id])
+    end
     def trip_params
       params.require(:trip).permit(:title, :description, :start_date, :end_date, :travel_mode, :ticket_cost, 
       :home_cab_cost, :destination_cab_cost, :hotel_cost, :local_transport)
