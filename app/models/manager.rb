8f35ba5cddd6e04a3deb5fa900b22480429c3a9d
X_NAVID <navidcaesar@gmail.com>
2018-02-18 21:42:58 +0000

    create managers table and model

---
+++ app/models/manager.rb
@@ -0,0 +1,14 @@
+class Manager < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 5, maximum: 25  }
+  # email format validation          
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+            
+  validates :email, presence: true, 
+            length: { maximum: 55 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218125248) do
+ActiveRecord::Schema.define(version: 20180218212858) do
+
+  create_table "managers", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string "title"
