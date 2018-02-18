b7fa6450b0b596ce0d2448bf3aa5a3da8c25e717
X_NAVID <navidcaesar@gmail.com>
2018-02-18 07:02:53 +0000

    create show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def show
+    @user = User.find(params[:id])
+  end
   def edit
     @user = User.find(params[:id])
   end
