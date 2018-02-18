4952c2bb1725096b4ed5cd52604e5afd00c69834
X_NAVID <navidcaesar@gmail.com>
2018-02-18 04:12:50 +0000

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217091449) do
+ActiveRecord::Schema.define(version: 20180218040706) do
 
   create_table "trips", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20180217091449) do
     t.decimal "destination_cab_cost"
     t.decimal "hotel_cost"
     t.string "local_transport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
