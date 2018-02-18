11b6b9d31f82204d15a7ac4def936f9113af1d5a
X_NAVID <navidcaesar@gmail.com>
2018-02-18 06:11:12 +0000

    Add secure password to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ActiveRecord::Base
             length: { maximum: 55 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :trips
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
