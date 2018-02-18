e08d5a0224c712c906f199e65c5da2b477f36ae4
X_NAVID <navidcaesar@gmail.com>
2018-02-18 11:56:00 +0000

    sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to travel portal #{@user.username}"
-      redirect_to trips_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
